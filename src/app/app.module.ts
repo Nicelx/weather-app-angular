0cc1e83ad40a3c97445acf2e277ae01c9271123b
Nicelx <relush321@gmail.com>
2023-10-01 23:38:09 +0300

    restructure a little bit, weather component-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppHomeComponent } from './app-home/app-home.component';
+import { AppHomeComponent } from './pages/app-home/app-home.component';
+import { WeatherComponent } from './pages/weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHomeComponent
+    AppHomeComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
