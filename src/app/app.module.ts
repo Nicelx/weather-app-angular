0a9a7a1a6f2639e0a429387b5346e8465957a5ea
Nicelx <relush321@gmail.com>
2023-10-23 23:59:34 +0300

    router module start routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppHomeComponent } from './pages/app-home/app-home.component';
 import { WeatherComponent } from './pages/weather/weather.component';
 import { WeatherItemComponent } from './pages/weather/weather-item/weather-item.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import { WeatherItemComponent } from './pages/weather/weather-item/weather-item.
     WeatherItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component:  AppHomeComponent},
+      { path: 'weather/:id', component:  WeatherComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
