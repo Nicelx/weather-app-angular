cf7d4404b48eb860358acfaa1345992137b4eb7a
Nicelx <relush321@gmail.com>
2023-12-20 23:58:33 +0300

    trying vanila debounce

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, map } from 'rxjs';
+
+function debounce(func:Function, timeout = 300){
+  let timer: any;
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+function saveInput(){
+  console.log('Saving data');
+}
+const processChange = debounce(() => saveInput());
+
 
 @Component({
   selector: 'app-weather',
@@ -13,7 +27,10 @@ export class WeatherComponent implements OnInit {
   }
 
   onKeyUp(event: Event) {
-    console.log('keyUp')
+    debounceTime(1000);
+
+    
+
   }
 
   setVisible(bool: boolean) {
