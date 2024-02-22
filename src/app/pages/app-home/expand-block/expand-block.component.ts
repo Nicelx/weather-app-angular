87934799db1e46f71fd6e442c53665e447e8ca81
Nicelx <relush321@gmail.com>
2024-02-22 23:40:20 +0300

    return to project, small styling changes

--- src/app/pages/app-home/expand-block/expand-block.component.ts
+++ src/app/pages/app-home/expand-block/expand-block.component.ts
@@ -9,6 +9,60 @@ export class ExpandBlockComponent implements OnInit {
 
   constructor() { }
 
+  items = [
+    {
+      time: '1 AM',
+      temperature: 13
+    },
+    {
+      time: '2 AM',
+      temperature: 13
+    },
+    {
+      time: ' 3AM',
+      temperature: 13
+    },
+    {
+      time: '4 AM',
+      temperature: 13
+    },
+    {
+      time: '5 AM',
+      temperature: 13
+    },
+    {
+      time: '6 AM',
+      temperature: 13
+    },
+    {
+      time: '7 AM',
+      temperature: 13
+    },
+    {
+      time: '8 AM',
+      temperature: 13
+    },
+    {
+      time: '9 AM',
+      temperature: 13
+    },
+    {
+      time: '10 AM',
+      temperature: 13
+    },
+    {
+      time: '11 AM',
+      temperature: 13
+    },
+    {
+      time: '12 AM',
+      temperature: 13
+    },
+    {
+      time: '1 PM',
+      temperature: 13
+    },
+  ]
   ngOnInit(): void {
   }
 
